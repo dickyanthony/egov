ce5ef79e1e326f1a1230ad308ba8f06740cc1352
dickyanthony <dickyanthony0812@gmail.com>
2023-05-18 19:03:56 +0700

    [DA] - Fix

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -34,9 +34,6 @@ const Hero = () => {
         <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full">
           Government.
         </h1>
-        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-          tesssssssssssssssssssssssssssssssssssssssssssss
-        </p>
       </div>
 
       <div
