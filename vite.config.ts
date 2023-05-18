7a34e3b3fad18b9231a3d020e0728f4b3793b5ac
dickyanthony <dickyanthony0812@gmail.com>
2023-05-18 17:39:48 +0700

    [DA] - Test

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/egov/",
+  base: "/egov",
   //@ts-ignore
   mimeTypes: {
     "text/javascript": ["js"],
